ab8f44f787cb30af33d511a496a7e9e3f6660789
Starinin Andrey <starininandrey@gmail.com>
2021-02-04 15:50:43 +0300

    Добавление полей в класс

--- person.h
+++ person.h
@@ -1,10 +1,22 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <QString>
+#include <QVector>
+
+#include "date.h"
+#include "address.h"
+#include "phone.h"
 
 class Person
 {
 public:
+    QString firstName;
+    QString lastName;
+    Date dateOfBirth;
+    Address address;
+    QVector<Phone> phones;
+
     Person();
 };
 

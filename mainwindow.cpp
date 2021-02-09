19c301a192bb436df69d05722b6075502b2a09b2
LetRuin <crot9000@yandex.ru>
2021-02-09 19:38:09 +0800

    добавлен контейнер для сохранения информации

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,30 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_button_clear_clicked()
+{
+    ui->input_firstNmae->clear();
+    ui->input_lastName->clear();
+    ui->input_dateOfBirth->setDate(QDate::currentDate());
+    ui->input_addressCountry->clear();
+    ui->input_addressRegion->clear();
+    ui->input_addressCity->clear();
+    ui->input_addressStreet->clear();
+    ui->input_addressHouse->clear();
+}
+
+void MainWindow::on_button_save_clicked()
+{
+    person.firstName = ui->input_firstNmae->text();
+    person.firstName = ui->input_lastName->text();
+    person.dateOfBirth = ui->input_dateOfBirth->date();
+    person.address.country = ui->input_addressCountry->text();
+    person.address.region = ui->input_addressRegion->text();
+    person.address.city = ui->input_addressCity->text();
+    person.address.street = ui->input_addressStreet->text();
+    person.address.house = ui->input_addressHouse->text();
+    Persons.push_back(person);
+
+    ui->statusbar->showMessage("Данные успешно сохранены");
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QDate>
+#include <QVector>
+
+#include "person.h"
+#include "phone.h"
+#include "address.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +21,13 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    QVector<Person> Persons;
+    Person person;
+
+private slots:
+    void on_button_clear_clicked();
+
+    void on_button_save_clicked();
 
 private:
     Ui::MainWindow *ui;
